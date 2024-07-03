4ef8bdd40f4bc66abdc405add31b4de467faf1ef
Evan Croft <evancroft13@gmail.com>
2024-07-03 13:52:04 -0400

    Added softdrop to HIJetReco and corresponding SD variables R_g and z_g to JetValidation

--- JS-Jet/JetValidation/macro/Fun4All_JetVal.C
+++ JS-Jet/JetValidation/macro/Fun4All_JetVal.C
@@ -14,7 +14,7 @@
 
 #include <g4centrality/PHG4CentralityReco.h>
 
-#include <HIJetReco.C>
+#include "HIJetReco_softdrop.C"
 #include <JetValidation.h>
 
 R__LOAD_LIBRARY(libfun4all.so)
@@ -45,28 +45,32 @@ void Fun4All_JetVal(const char *filelisttruth = "dst_truth_jet.list",
   se->registerSubsystem( cent );
   
   Enable::VERBOSITY = verbosity;
-  HIJetReco();
+  HIJetReco_softdrop();
 
-  JetValidation *myJetVal = new JetValidation("AntiKt_Tower_r04_Sub1", "AntiKt_Truth_r04", outname);
+  JetValidation *myJetVal = new JetValidation("AntiKt_TowerInfo_Unsubtracted_r04", "AntiKt_Truth_r04", outname);
 
   myJetVal->setPtRange(5, 100);
   myJetVal->setEtaRange(-1.1, 1.1);
   myJetVal->doUnsub(1);
-  myJetVal->doTruth(1);
+  myJetVal->doTruth(0);
   myJetVal->doSeeds(1);
   se->registerSubsystem(myJetVal);
+
+  //We need to remove all DST inputs other than DSTcalo for data
  
+  /*
   Fun4AllInputManager *intrue = new Fun4AllDstInputManager("DSTtruth");
   intrue->AddListFile(filelisttruth,1);
-  se->registerInputManager(intrue);
+  se->registerInputManager(intrue);*/
 
   Fun4AllInputManager *in2 = new Fun4AllDstInputManager("DSTcalo");
   in2->AddListFile(filelistcalo,1);
   se->registerInputManager(in2);
 
+  /*
   Fun4AllInputManager *in3 = new Fun4AllDstInputManager("DSTglobal");
   in3->AddListFile(filelistglobal,1);
-  se->registerInputManager(in3);
+  se->registerInputManager(in3);*/
   
   se->run(-1);
   se->End();
